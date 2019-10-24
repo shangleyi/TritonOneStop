439fef4cf8d23a28caa47b835762b2dc13072c7c
WeifanOu <ouweifanandy@outlook.com>
2019-10-24 14:36:04 -0700

    reorgnized some files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import TOSNavBar from './Components/TOSNavBar'
+import TOSNavBar from './components/TOSNavBar'
 
 function App() {
   return (
