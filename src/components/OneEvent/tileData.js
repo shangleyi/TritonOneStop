5b46eb53ccc76085a21152a3b1ab334bfdfca846
zijingdi <zijingdi@sohu-inc.com>
2019-10-29 15:32:53 -0700

    modify

--- src/components/OneEvent/tileData.js
+++ src/components/OneEvent/tileData.js
@@ -1,27 +1,42 @@
 import image from './5230508_orig.jpg';
+import piazza from './Piazza-Icon.png';
+import BKGND from './WebsiteBKGND.png';
+import canvas from './canvas.png';
+import careerCenter from './career-center.png';
+import gradescope from './career-center.png';
+import iclicker from './career-center.png';
+import parking from './career-center.png';
+import rimac from './career-center.png';
+import shs from './career-center.png';
+import shuttle from './career-center.png';
+import studentOrg from './career-center.png';
+import studyAbroad from './career-center.png';
+import foodPantry from './career-center.png';
+import dining from './career-center.png';
+import research from './career-center.png';
 export const tileData = [
     {
         img: image,
         title: "zzzzzz",
     },
     {
-        img: "./Piazza-Icon.png",
-        title: "zzzzzz",
+        img: piazza,
+        title: "piazza",
     },
     {
-        img: "./WebsiteBKGND.png",
-        title: "zzzzzz",
+        img: BKGND,
+        title: "BKGND",
     },
     {
-        img: "./canvas.png",
-        title: "zzzzzz",
+        img: canvas,
+        title: "canvas",
     },
     {
-        img: "../resources/career-center.jpg",
-        title: "zzzzzz",
+        img: careerCenter,
+        title: "career-center",
     },
     {
-        img: "../resources/financial-aid.png",
+        img: "financial-aid.png",
         title: "zzzzzz",
     },
 ];
