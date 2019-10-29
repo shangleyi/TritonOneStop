f09940cdf29acf3df13750cf159d57b49dfab70c
zijingdi <zijingdi@sohu-inc.com>
2019-10-29 15:20:12 -0700

    add gird

--- src/components/OneEvent/tileData.js
+++ src/components/OneEvent/tileData.js
@@ -1,6 +1,27 @@
-const tileData = [
+import image from './5230508_orig.jpg';
+export const tileData = [
     {
-        img: "resources/5230508_orig.jpg"
-        title: "zzzzzz"
+        img: image,
+        title: "zzzzzz",
     },
-]
+    {
+        img: "./Piazza-Icon.png",
+        title: "zzzzzz",
+    },
+    {
+        img: "./WebsiteBKGND.png",
+        title: "zzzzzz",
+    },
+    {
+        img: "./canvas.png",
+        title: "zzzzzz",
+    },
+    {
+        img: "../resources/career-center.jpg",
+        title: "zzzzzz",
+    },
+    {
+        img: "../resources/financial-aid.png",
+        title: "zzzzzz",
+    },
+];
--- src/routes/Resource.js
+++ src/routes/Resource.js
@@ -1,16 +1,81 @@
 import React, {Component} from 'react';
 import {Redirect} from "react-router-dom";
-import NarBar from "../components/NavBar/TOSNavBar"
+import NavBar from "../components/NavBar/TOSNavBar"
+import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import IconButton from '@material-ui/core/IconButton';
+import InfoIcon from '@material-ui/icons/Info';
+import {tileData} from '../components/OneEvent/tileData';
 
+// const useStyles = makeStyles((theme: Theme) =>
+//   createStyles({
+//     root: {
+//       display: 'flex',
+//       flexWrap: 'wrap',
+//       justifyContent: 'space-around',
+//       overflow: 'hidden',
+//       backgroundColor: theme.palette.background.paper,
+//     },
+//     gridList: {
+//       width: 500,
+//       height: 450,
+//     },
+//     icon: {
+//       color: 'rgba(255, 255, 255, 0.54)',
+//     },
+//   }),
+// );
+// function setStyle() {
+//     const classes =  useStyles();
+// }
 class Resource extends Component{
     constructor(props){
         super(props)
     }
 
     render() {
+        const classes = makeStyles((theme: Theme) =>
+            createStyles({
+                root: {
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    justifyContent: 'space-around',
+                    overflow: 'hidden',
+                    backgroundColor: theme.palette.background.paper,
+                },
+                gridList: {
+                    width: 500,
+                    height: 450,
+                },
+                icon: {
+                    lor: 'rgba(255, 255, 255, 0.54)',
+                },
+            }),
+        );
         return (
-            <div>
-                <NarBar/>
+            <div className={classes.root}>
+                <NavBar/>
+                <GridList cellHeight={180} className={classes.gridList}>
+                    <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
+                        <ListSubheader component="div">Resources</ListSubheader>
+                    </GridListTile>
+                    {tileData.map(tile => (
+                    <GridListTile key={tile.img}>
+                        <img src={tile.img} alt={tile.title} />
+                        <GridListTileBar
+                            title={tile.title}
+                            actionIcon={
+                                <IconButton aria-label={`info about ${tile.title}`} className={classes.icon}>
+                                <InfoIcon />
+                                </IconButton>
+                            }
+                        />
+                    </GridListTile>
+                    ))}
+                </GridList>
             </div>
         )
     }
