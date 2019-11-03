518d7278f3cebf196a1abb86062f9facf5bfa67e
v0vbshi <46660694+v0vbshi@users.noreply.github.com>
2019-11-03 15:36:20 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import TOSNavBar from './components/NavBar/TOSNavBar'
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import Resource from "./routes/Resource";
+import Main from "./routes/Main";
+import Events from "./routes/Events";
+import CoursePlanner from "./routes/CoursePlanner"
+
+function App() {
+  return (
+    // <div className="App">
+    //   <TOSNavBar />
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <Router>
+        <div>
+          <Route exact path="/resource" component={Resource}/>
+          <Route exact path="/" component={Main}/>
+          <Route exact path="/event" component={Events}/>
+          <Route exact path="/courseplanner" component={CoursePlanner}/>
+        </div>
+    </Router>
+  );
+}
+
+export default App;
