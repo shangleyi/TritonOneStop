7332e61975e05749c2795070d137805f00282ad5
WeifanOu <ouweifanandy@outlook.com>
2019-11-10 13:37:56 -0800

    login logout should be done, need to debug and work on forget password

--- src/components/NavBar/TOSNavBar.js
+++ src/components/NavBar/TOSNavBar.js
@@ -7,9 +7,21 @@ import db_login from "../../database/base";
 
 
 class TOSNavBar extends React.Component {
+    /*
+        Important data fields:
+        ├── this.state:
+        |   ├── isLoggedIn: Used to determine UI rendering,
+        |   |               initialized according to local storage when starting, will be updated when login and logout.
+        |   ├── userName:   Used to pass userName from input to signup and login
+        |   └── userPW:     Used to pass PW from input to signup and login
+        |
+        ├── localStorage:   Stores userName locally in case user refreshes the page, will only be updated when login and
+        |                   logout.
+        └── db_login.auth(): Firebase authentication status, comes with a bit of delay, careful when using it
+     */
+
     constructor(props) {
         super(props);
-        this.state={isLoggedIn: false, userName: null, userPW: null};
         this.handleClickResource = this.handleClickResource.bind(this);
         this.handleClickEvent = this.handleClickEvent.bind(this);
         this.handleClickMain = this.handleClickMain.bind(this);
@@ -18,55 +30,86 @@ class TOSNavBar extends React.Component {
         this.handleSignUp = this.handleSignUp.bind(this);
         this.handleLogOut = this.handleLogOut.bind(this);
         this.handleLogInError = this.handleLogInError.bind(this);
+        console.log("Current User at construct:");
+        console.log(db_login.auth().currentUser);
+        if(localStorage.getItem("UserName")==="null") {
+            this.state={isLoggedIn: false, userName: null, userPW: null};
+        }
+        else {
+            this.state={isLoggedIn: true, userName: localStorage.getItem("UserName"), userPW: ""};
+        }
     }
 
     handleLogInError(error) {
         alert(error);
         this.setState({isLoggedIn: false});
+        console.log("NavBar: Log-In: Fake TRUE, FALSE");
+        localStorage.setItem("UserName", null);
     }
 
     handleLogIn(){
-        console.log(this.state);
+        console.log("HandleLogIn: ATTEMPT");
+        /*
+        console.log("Handle Login: Before Login:", "\nUser ID: ", db_login.auth().currentUser.uid,
+            "\nLocalStorage: ", localStorage.getItem("UserName"));
+
+         */
+
+        // Actual login
         try{
-            db_login.auth().signInWithEmailAndPassword(this.state.userName, this.state.userPW).catch(this.handleLogInError);
             console.log("NavBar: Log-In: TRUE");
             this.setState({isLoggedIn: true});
-
+            db_login.auth().signInWithEmailAndPassword(this.state.userName, this.state.userPW).catch(this.handleLogInError);
+            localStorage.setItem("UserName", this.state.userName);
         } catch (error) {
             alert(error);
-            console.log("Failed to login");
+            console.log("NavBar: Log-In: FALSE");
         }
     }
 
     handleSignUp(){
+        console.log("handleSignUp: ATTEMPT");
+        /*
+        console.log("Handle SignUp: Before SignUp:", "\nUser ID: ", db_login.auth().currentUser.uid,
+            "\nLocalStorage: ", localStorage.getItem("UserName"));
+
+         */
         try{
             db_login.auth().createUserWithEmailAndPassword(this.state.userName, this.state.userPW);
             console.log("NavBar: SignUp");
             alert("Signed Up with email" + this.state.userName)
         } catch (error) {
             alert(error);
-            console.log("Failed to login");
+            console.log("Failed to SignUp");
         }
+        /*
+        console.log("Handle SignUp: After SignUp:", "\nUser ID: ", db_login.auth().currentUser.uid,
+            "\nLocalStorage: ", localStorage.getItem("UserName"));
+            
+         */
+    }
+
+    handleLogOut(){
+        console.log("HandleLogOut: ATTEMPT");
+        console.log("Handle LogOut: Before Logout:", "\nUser ID: ", db_login.auth().currentUser.uid,
+            "\nLocalStorage: ", localStorage.getItem("UserName"));
+        console.log("NavBar: Log-In: FALSE");
+
+        db_login.auth().signOut();
+        localStorage.setItem("UserName", null);
+        this.setState({isLoggedIn: false});
+
+        console.log("Handle LogOut: After Logout:", "\nUser ID: ", db_login.auth().currentUser.uid,
+            "\nLocalStorage: ", localStorage.getItem("UserName"));
     }
 
     handleUsernameChange = (event) => {
-        //console.log("New UserName:" + event.target.value);
         this.setState({userName: event.target.value});
-        //console.log(this.state)
     };
 
     handlePWChange = (event) => {
-        //console.log("New PW:" + event.target.value);
         this.setState({userPW: event.target.value});
     };
-
-    handleLogOut(){
-        console.log("NavBar: Log-In: FALSE");
-        console.log(db_login.auth());
-        db_login.auth().signOut();
-        console.log(db_login.auth());
-        this.setState({isLoggedIn: false});
-    }
 
     handleClickResource = () => {
         this.props.history.push("/resource");
