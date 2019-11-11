4832778381123d6b02a38bc617babd48c78f09ff
qixinliu <46657740+qixinliu@users.noreply.github.com>
2019-11-10 16:24:23 -0800

    Add more info on MainSquareMap.js for learnmore

--- src/components/MainPageSquare/MainSquareMap.js
+++ src/components/MainPageSquare/MainSquareMap.js
@@ -13,44 +13,53 @@ export const MainSquareMap = [
     {
         img: myTriLink,
         title: "My Triton Link",
-        URL: "https://act.ucsd.edu/myTritonlink20/display.htm"
+        URL: "https://act.ucsd.edu/myTritonlink20/display.htm",
+        More: "https://students.ucsd.edu/sponsor/new-students/before-arrive-freshmen.html"
     },
     {
         img: Podcast,
         title: "Podcast",
-        URL: "https://podcast.ucsd.edu/"
+        URL: "https://podcast.ucsd.edu/",
+        More: "https://podcast.ucsd.edu/howto.shtml"
+
     },
     {
         img: piazza,
         title: "Piazza",
-        URL: "https://piazza.com"
+        URL: "https://piazza.com",
+        More: "https://www.youtube.com/watch?v=2jLSiN8E18w"
     },
     {
         img: gradescope,
         title: "Gradescope",
-        URL: "https://www.gradescope.com"
+        URL: "https://www.gradescope.com",
+        More: "https://www.youtube.com/watch?v=KMPoby5g_nE&feature=youtu.be"
     },
     {
         img: canvas,
         title: "Canvas",
-        URL: "https://coursefinder.ucsd.edu"
+        URL: "https://coursefinder.ucsd.edu",
+        More: "https://www.instructure.com/canvas/"
     },
     
     {
         img: gmail,
         title: "Gmail",
-        URL: "https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin"
+        URL: "https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin",
+        More: "https://google.com"
     },
     {
         img: webreg,
         title: "Online Enrollment",
-        URL: "https://act.ucsd.edu/webreg2/start"
+        URL: "https://act.ucsd.edu/webreg2/start",
+        More: "https://students.ucsd.edu/academics/enroll/index.html"
     },
     
     {
         img: handshake,
         title: "HandShake(UCSD LINKIN)",
-        URL: "https://ucsd.joinhandshake.com/login"
+        URL: "https://ucsd.joinhandshake.com/login",
+        More: "https://www.joinhandshake.com/students/how-it-works/"
     },
     {
         img: plus,
