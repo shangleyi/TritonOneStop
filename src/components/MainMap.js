0e7241a3591f90d0c70172624b6f9550f2d40696
qixinliu <46657740+qixinliu@users.noreply.github.com>
2019-11-10 16:22:28 -0800

    modify MainMap.js and it function ideally.

--- src/components/MainMap.js
+++ src/components/MainMap.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import {Redirect} from 'react-router-dom';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
@@ -7,12 +8,19 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import {flexbox} from "@material-ui/system";
+import { Link, ListItem } from '@material-ui/core';
+
 
 const useStyles = makeStyles({
   card: {
     maxWidth: 345,
-  },
+    paddingLeft:  20,
+    paddingRight: 20,
+    display: flexbox,
+  }
 });
+
 
 export default function ImgMediaCard(props) {
   const classes = useStyles();
@@ -21,28 +29,30 @@ export default function ImgMediaCard(props) {
   return (
     <Card className={classes.card}>
       <CardActionArea>
-        <CardMedia
-          component="img"
-          alt={tile.title}
-          height="140"
-          image={tile.img}
-          title={tile.title}
-        />
+      
+          <Button target="_blank" href={tile.URL}>
+            <CardMedia
+              component="img"
+              alt={tile.title}
+              height="140"
+              image={tile.img}
+              title={tile.title}
+             />
+            </Button>
+        
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
             {tile.title}
-          </Typography>
-          
+          </Typography>          
         </CardContent>
       </CardActionArea>
       <CardActions>
-        <Button size="small" color="primary">
+        <ListItem primaryText = "Edit" value="Edit" linkButton={true} href='https://www.google.com'>
           Edit
-        </Button>
-        <Button size="small" color="primary">
+        </ListItem>
+        <Button target="_blank" href={tile.More} size="small" color="primary" > 
           Learn More
         </Button>
       </CardActions>
     </Card>
-    );
-}
+    );}
