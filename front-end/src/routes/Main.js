9cd5478d9c8b98f4b65e71272b9fa711494e60c1
v0vbshi <46660694+v0vbshi@users.noreply.github.com>
2019-11-18 20:55:37 -0800

    change mainpage design

--- front-end/src/routes/Main.js
+++ front-end/src/routes/Main.js
@@ -4,22 +4,49 @@ import TOSNavBar from "../components/NavBar/TOSNavBar";
 import '../App.css';
 import {MainSquareMap} from "../components/MainPageSquare/MainSquareMap";
 import GridList from "@material-ui/core/GridList";
-import GridListTile from "@material-ui/core/GridListTile";
-import ListSubheader from "@material-ui/core/ListSubheader";
 import MainMap from "../components/MainMap.js";
+import {db, firebase} from '../base'
 
 class Main extends Component {
     constructor(props) {
-        super(props)
+        super(props);
+
+        this.state = {
+            userName: "Please Log In to display User Name"
+        };
+
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                // User logged in already or has just logged in.
+                this.setState({userName:user.email.substring(0, user.email.indexOf('@'))});
+            } else {
+                // User not logged in or has just logged out.
+                this.state.userName = "Please Log in to display user name";
+            }});
     }
 
     render() {
         return (
-            <div className="App">
+            <div className="mainPage-background">
                 <TOSNavBar/>
-                <div className="background"/>
+                <h1 className="welcomeStyle">
+                    Welcome to
+                </h1>
+                <h2 className="mainPageH2">
+                    Triton OneStop
+                </h2>
+                <h3 className="mainPageH3">
+                    {this.state.userName}
+                </h3>
+                <h4 className="mainPageH4">
+                    This is your OneStop Guide to Know about UC San Diego
+                </h4>
+                <p>
+                    <i className="arrow down"></i>
+                </p>
                 <div className="App-mainPageLayout">
-                    <GridList style={{marginLeft: 100, marginRight: 'auto'}} cellHeight={180}>
+                    <GridList style={{marginLeft: 60, marginRight: 'auto'}} cellHeight={180}>
+                    {/*<GridList style={{alignContent: "center"}} cellHeight={180}>*/}
                         {MainSquareMap.map(tile => (
                             <MainMap key={tile} tile={tile}/>
                         ))}
@@ -27,7 +54,6 @@ class Main extends Component {
                 </div>
             </div>
         )
-
     }
 }
 export default Main;
